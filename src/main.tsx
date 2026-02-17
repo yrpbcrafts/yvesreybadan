325ba5cbfdb7a4465ed545923c4a323571d2b506
Boss Yves <nii-yrpbadan@bpims.com>
2026-02-17 18:40:06 +0800

    Removed basename

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/yvesreybadan">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
